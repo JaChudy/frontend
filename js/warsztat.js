fbe2a129da09fa63955d8787c88bd72e4822a16c
JaChudy <jakub.chudy@gmail.com>
2018-08-10 15:14:39 +0200

    zadanie 3

--- js/warsztat.js
+++ js/warsztat.js
@@ -1,7 +1,29 @@
 $(function () {
 
     function render(books) {
-        console.log("ok");
+
+        var booksElement = $('#books');
+        booksElement.empty();
+
+        books.forEach(function (book) {
+            var title = $("<div>" + book.title + "</div>");
+            booksElement.append(title);
+
+            var bookDetails = $("<div>");
+
+            title.after(bookDetails);
+            bookDetails.text("tu wstawimy szczegóły dotyczące naszej ksiązki");
+            bookDetails.addClass("hidden");
+
+            title.click(function () {
+                bookDetails.toggle("hidden");
+            });
+
+
+
+
+
+        });
     }
 
     $.get("http://localhost:8282/books")
